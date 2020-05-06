35ba36becf5e1011adf9f3022fd577b56c5fef4e
HakobChalikyan <hakobchalikyan@yahoo.com>
2020-05-06 14:10:16 -0700

    message

--- src/edu/ucsd/cs110/tests/CelsiusTest.java
+++ src/edu/ucsd/cs110/tests/CelsiusTest.java
@@ -2,21 +2,17 @@ package edu.ucsd.cs110.tests;
 import edu.ucsd.cs110.temperature.Celsius;
 import edu.ucsd.cs110.temperature.Temperature;
 import junit.framework.TestCase;
-import org.junit.Assert;
-import org.junit.Test;
 
-public class CelsiusTest {
+public class CelsiusTest extends TestCase{
     private float delta = 0.001f;
 
-    @Test
     public void testCelsius(){
         float value = 12.34f;
         Celsius temp = new Celsius(value);
 
-        Assert.assertEquals(value, temp.getValue(), delta);
+        assertEquals(value, temp.getValue(), delta);
     }
 
-    @Test
     public void testCelsiusToString(){
         float value = 12.34f;
 
@@ -27,36 +23,34 @@ public class CelsiusTest {
         String ending = " C";
 
         // Verify the suffix of the formatted string
-        Assert.assertTrue(string.startsWith(beginning));
+        assertTrue(string.startsWith(beginning));
 
         // Verify the prefix of the formatted string
-        Assert.assertTrue(string.endsWith(ending));
+        assertTrue(string.endsWith(ending));
 
         // Verify the middle of the formatted string
         int endIndex = string.indexOf(ending);
 
         // (Hint: what is the length of the middle of the string?)
-        Assert.assertTrue(string.substring(0, endIndex).equals(beginning));
+        assertTrue(string.substring(0, endIndex).equals(beginning));
     }
 
-    @Test
     public void testCelsiusToCelsius()
     {
         Celsius temp = new Celsius(0);
         Temperature convert = temp.toCelsius();
-        Assert.assertEquals(0, convert.getValue(), delta);
+        assertEquals(0, convert.getValue(), delta);
     }
 
-    @Test
     public void testCelsiusToFahrenheit(){
         Celsius temp = new Celsius(0);
 
         Temperature convert = temp.toFahrenheit();
-        Assert.assertEquals(32, convert.getValue(), delta);
+        assertEquals(32, convert.getValue(), delta);
 
         temp = new Celsius(100);
         convert = temp.toFahrenheit();
 
-        Assert.assertEquals(212, convert.getValue(), delta);
+        assertEquals(212, convert.getValue(), delta);
     }
 }
--- src/edu/ucsd/cs110/tests/FahrenheitTest.java
+++ src/edu/ucsd/cs110/tests/FahrenheitTest.java
@@ -2,21 +2,17 @@ package edu.ucsd.cs110.tests;
 import edu.ucsd.cs110.temperature.Fahrenheit;
 import edu.ucsd.cs110.temperature.Temperature;
 import junit.framework.TestCase;
-import org.junit.Assert;
-import org.junit.Test;
 
-public class FahrenheitTest {
+public class FahrenheitTest extends TestCase{
     private float delta = 0.001f;
 
-    @Test
     public void testFahrenheit(){
         float value = 12.34f;
         Fahrenheit temp = new Fahrenheit(value);
 
-        Assert.assertEquals(value, temp.getValue(), delta);
+        assertEquals(value, temp.getValue(), delta);
     }
 
-    @Test
     public void testFahrenheitToString(){
         float value = 12.34f;
 
@@ -27,37 +23,35 @@ public class FahrenheitTest {
         String ending = " F";
 
         // Verify the suffix of the formatted string
-        Assert.assertTrue(string.startsWith(beginning));
+        assertTrue(string.startsWith(beginning));
 
         // Verify the prefix of the formatted string
-        Assert.assertTrue(string.endsWith(ending));
+        assertTrue(string.endsWith(ending));
 
         // Verify the middle of the formatted string
         int endIndex = string.indexOf(ending);
 
         // (Hint: what is the length of the middle of the string?)
-        Assert.assertTrue(string.substring(0, endIndex).equals(beginning));
+        assertTrue(string.substring(0, endIndex).equals(beginning));
     }
 
-    @Test
     public void testFahrenheitToFahrenheit()
     {
         Fahrenheit temp = new Fahrenheit(32);
 
         Temperature convert = temp.toFahrenheit();
-        Assert.assertEquals(32, convert.getValue(), delta);
+        assertEquals(32, convert.getValue(), delta);
     }
 
-    @Test
     public void testFahrenheitToCelsius(){
         Fahrenheit temp = new Fahrenheit(32);
 
         Temperature convert = temp.toCelsius();
-        Assert.assertEquals(0, convert.getValue(), delta);
+        assertEquals(0, convert.getValue(), delta);
 
         temp = new Fahrenheit(212);
         convert = temp.toCelsius();
 
-        Assert.assertEquals(100, convert.getValue(), delta);
+        assertEquals(100, convert.getValue(), delta);
     }
 }
